ec0f9fa286ff6a993d3ce8c1511170240f516601
Peter Burns <rictic@google.com>
2014-01-26 15:45:49 -0800

    facing

--- player.js
+++ player.js
@@ -5,6 +5,7 @@ function Player () {
   this.sprite = new THREE.Sprite(dudeMaterial);
   this.sprite.position.set(0, 100, 0);
   this.sprite.scale.set(4*13, 4*21, 1.0); // imageWidth, imageHeight
+  this.direction = -1;
 };
 
 Player.prototype.tick = function() {
@@ -16,16 +17,23 @@ Player.prototype.tick = function() {
   // apply speed to position
   this.sprite.position.x += this.speedX;
 
+  // facing
+  if ((this.direction == -1 && this.speedX > 0) ||
+      (this.direction == 1 && this.speedX < 0)) {
+    this.sprite.scale.x *= -1;
+    this.direction *= -1
+  }
+
   // friction
   if (this.speedX) {
-	  this.speedX *= 0.97;
-	  if (Math.abs(this.speedX) < 0.50) {
-	  	this.speedX = 0;
-	  }
+    this.speedX *= 0.97;
+    if (Math.abs(this.speedX) < 0.50) {
+      this.speedX = 0;
+    }
   }
 };
 
 Player.prototype.jump = function() {
-	var JUMP_HEIGHT = 10;
-	this.sprite.position.y += JUMP_HEIGHT;
+  var JUMP_HEIGHT = 10;
+  this.sprite.position.y += JUMP_HEIGHT;
 };
