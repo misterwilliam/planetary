14cb876a5c1d61cc248260a2638d323642129425
Alexander Marks <aomarks@gmail.com>
2014-01-26 17:15:51 -0800

    plant decay

--- plant.js
+++ plant.js
@@ -1,19 +1,35 @@
 
+var GREEN_MATERIAL = new THREE.SpriteMaterial({
+  map: THREE.ImageUtils.loadTexture('images/plant.png')
+});
+
+var BROWN_MATERIAL = new THREE.SpriteMaterial({
+  map: THREE.ImageUtils.loadTexture('images/plant-brown.png')
+});
+
+var BLACK_MATERIAL = new THREE.SpriteMaterial({
+  map: THREE.ImageUtils.loadTexture('images/plant-black.png')
+});
+
 function Plant(x, y) {
   this.x = x;
   this.y = y;
-  var texture = THREE.ImageUtils.loadTexture('images/plant.png');
-  var material = new THREE.SpriteMaterial({ map: texture });
-  this.sprite = new THREE.Sprite(material);
+  this.sprite = new THREE.Sprite(GREEN_MATERIAL);
   this.sprite.position.set(x * 64, y * 64, 0);
   this.sprite.scale.set(64, 64, 1.0);
   this.ticksSinceLastDrop = 0;
+  this.ticksSinceLastDecay = 0;
+  this.life = 100;
 };
 
 Plant.prototype.tick = function() {
   if (++this.ticksSinceLastDrop == 60) {
     this.dropWater();
     this.ticksSinceLastDrop = 0;
+  }
+  if (++this.ticksSinceLastDecay == 60 * 5) {
+    this.decay(20);
+    this.ticksSinceLastDecay = 0;
   }
 };
 
@@ -27,4 +43,13 @@ Plant.prototype.dropWater = function() {
       ground.water(20);
     }
   });
+};
+
+Plant.prototype.decay = function(amt) {
+  this.life -= amt;
+  if (this.life <= 0) {
+    this.sprite.material = BLACK_MATERIAL;
+  } else if (this.life <= 50) {
+    this.sprite.material = BROWN_MATERIAL;
+  }
 };
