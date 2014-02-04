bf4836e38a961e70d5d85db18cf67d07421f6b6e
Peter Burns <rictic@google.com>
2014-02-04 09:15:33 -0800

    Plant to class

--- plant.ts
+++ plant.ts
@@ -3,47 +3,46 @@ var GREEN_MATERIAL = LoadJaggyMaterial('images/plant.png');
 var BROWN_MATERIAL = LoadJaggyMaterial('images/plant-brown.png');
 var BLACK_MATERIAL = LoadJaggyMaterial('images/plant-black.png');
 
-function Plant(x, y) {
-  this.x = x;
-  this.y = y;
-  this.sprite = new THREE.Sprite(GREEN_MATERIAL);
-  var lc = game.blockToLocal(x, y);
-  this.sprite.position.set(lc[0], lc[1], 0);
-  this.sprite.scale.set(64, 64, 1.0);
-  this.ticksSinceLastDrop = 0;
-  this.ticksSinceLastDecay = 0;
-  this.life = 100;
-};
-
-Plant.prototype.tick = function() {
-  if (++this.ticksSinceLastDrop == 60) {
-    this.dropWater();
-    this.ticksSinceLastDrop = 0;
+class Plant {
+  sprite = new THREE.Sprite(GREEN_MATERIAL);
+  ticksSinceLastDrop = 0;
+  ticksSinceLastDecay = 0;
+  life = 100;
+  constructor(public x:number, public y:number) {
+    var lc = game.blockToLocal(x, y);
+    this.sprite.position.set(lc[0], lc[1], 0);
+    this.sprite.scale.set(64, 64, 1.0);
   }
-  if (++this.ticksSinceLastDecay == 60 * 5) {
-    this.decay(20);
-    this.ticksSinceLastDecay = 0;
-  }
-};
 
-Plant.prototype.dropWater = function() {
-  var neighborCoords = Grid.entityNeighbors(this, 2);
-  neighborCoords.forEach(function(coord) {
-    var x = coord[0];
-    var y = coord[1];
-    var ground = game.terrainGrid[[x,y]];
-    if (ground) {
-      ground.water(20);
+  tick() {
+    if (++this.ticksSinceLastDrop == 60) {
+      this.dropWater();
+      this.ticksSinceLastDrop = 0;
     }
-  });
-};
-
-Plant.prototype.decay = function(amt) {
-  this.life -= amt;
-  if (this.life <= 0) {
-    this.sprite.material = BLACK_MATERIAL;
-  } else if (this.life <= 50) {
-    this.sprite.material = BROWN_MATERIAL;
+    if (++this.ticksSinceLastDecay == 60 * 5) {
+      this.decay(20);
+      this.ticksSinceLastDecay = 0;
+    }
   }
-};
 
+  dropWater = function() {
+    var neighborCoords = Grid.entityNeighbors(this, 2);
+    neighborCoords.forEach(function(coord) {
+      var x = coord[0];
+      var y = coord[1];
+      var ground = game.terrainGrid[[x,y]];
+      if (ground) {
+        ground.water(20);
+      }
+    });
+  }
+
+  decay(amt) {
+    this.life -= amt;
+    if (this.life <= 0) {
+      this.sprite.material = BLACK_MATERIAL;
+    } else if (this.life <= 50) {
+      this.sprite.material = BROWN_MATERIAL;
+    }
+  }
+}
