6e949d34fe58638c390d2609a8f656786b0a6eea
Peter Burns <rictic@google.com>
2014-02-04 09:03:44 -0800

    background to class

--- background.ts
+++ background.ts
@@ -1,15 +1,14 @@
 var BACKGROUND_TEXTURE = THREE.ImageUtils.loadTexture("images/mountains.png");
 var BACKGROUND_MATERIAL = new THREE.MeshBasicMaterial({map: BACKGROUND_TEXTURE});
 
-function BackgroundController(scene) {
-  this.scene = scene;
-};
-
-BackgroundController.prototype.drawBackground = function() {
-  for (var x = -30; x < 30; x++) {
-    var meshBg = new THREE.Mesh(new THREE.PlaneGeometry(640 * 2, 640 * 2),
-      BACKGROUND_MATERIAL);
-    meshBg.position.set(x * 640 * 2, 640 -100, -200 );
-    this.scene.add(meshBg);
-  }
-};
+class BackgroundController {
+	constructor(public scene) {}
+	drawBackground() {
+	  for (var x = -30; x < 30; x++) {
+	    var meshBg = new THREE.Mesh(new THREE.PlaneGeometry(640 * 2, 640 * 2),
+	      BACKGROUND_MATERIAL);
+	    meshBg.position.set(x * 640 * 2, 640 -100, -200 );
+	    this.scene.add(meshBg);
+	  }
+	}
+}
