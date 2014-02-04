b2f0d8a0b37a785b57c68e0ce922b2e8a72a6088
Peter Burns <rictic@google.com>
2014-02-04 09:36:05 -0800

    make Game and Player into classes too

--- game.ts
+++ game.ts
@@ -29,6 +29,8 @@ var getNow = (function() {
   return function(){return +new Date()};
 })();
 
+var tickCount = 0;
+
 // Creates a new SpriteMaterial with nearest-neighbor texture filtering from
 // image URL.
 function LoadJaggyMaterial(url) {
@@ -37,308 +39,310 @@ function LoadJaggyMaterial(url) {
   return new THREE.SpriteMaterial({map: texture});
 };
 
-function Game() {
-  this.scene = new THREE.Scene();
-  this.camera = new THREE.PerspectiveCamera(90, null, 0.1, 1000);
-  this.camera.position.set(0, 0, 800);
-  this.renderer = new THREE.WebGLRenderer();
-  this.resize();
-  document.body.appendChild(this.renderer.domElement);
 
-  this.now = getNow();
-  this.lastTime = getNow();
-  this.unprocessedFrames = 0;
-  this.input = {
+class Game {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(90, null, 0.1, 1000);
+  renderer = new THREE.WebGLRenderer();
+  now = getNow();
+  lastTime = getNow();
+  unprocessedFrames = 0;
+  input = {
     jump: false, down: false, right: false, left: false, dig:false
   };
+  lastEntityId = -1;
+  entities = [];
+  terrainGrid = new Grid();
+  debug = false;
+  groundPlane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
+  projector = new THREE.Projector();
+  atmosphereController = new AtmosphereController(this.scene);
+  player : Player;
+  plants : Plant[];
+  debugSprites : THREE.Object3D[];
 
-  this.lastEntityId = -1;
-  this.entities = [];
-  this.terrainGrid = new Grid();
-  this.debug = false;
-  this.groundPlane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
-  this.projector = new THREE.Projector();
-  this.atmosphereController = new AtmosphereController(this.scene);
-};
-
-Game.prototype.resize = function() {
-  this.camera.aspect = window.innerWidth / window.innerHeight;
-  this.camera.updateProjectionMatrix();
-  this.renderer.setSize(window.innerWidth, window.innerHeight);
-};
-
-Game.prototype.handleInput = function() {
-  if (this.input.jump) {
-    this.player.jump();
-  } else if (this.input.down) {
-    // do nothing
+  constructor() {
+    this.camera.position.set(0, 0, 800);
+    this.resize();
+    document.body.appendChild(this.renderer.domElement);
   }
 
-  if (this.input.right) {
-    this.player.speedX += PLAYER_ACCELERATION;
-    this.player.speedX = Math.max(
-        this.player.speedX, PLAYER_MAX_SPEED);
-  }  else if (this.input.left) {
-    this.player.speedX -= PLAYER_ACCELERATION;
-    this.player.speedX = Math.min(
-        this.player.speedX, -PLAYER_MAX_SPEED);
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
-  if (this.input.dig) {
-    this.player.dig();
-  }
-};
+  handleInput() {
+    if (this.input.jump) {
+      this.player.jump();
+    } else if (this.input.down) {
+      // do nothing
+    }
 
+    if (this.input.right) {
+      this.player.speedX += PLAYER_ACCELERATION;
+      this.player.speedX = Math.max(
+          this.player.speedX, PLAYER_MAX_SPEED);
+    }  else if (this.input.left) {
+      this.player.speedX -= PLAYER_ACCELERATION;
+      this.player.speedX = Math.min(
+          this.player.speedX, -PLAYER_MAX_SPEED);
+    }
 
-
-Game.prototype.handleKey = function(event) {
-  var key = INPUT_MAP[event.which];
-  if (!key) {
-    console.log('unbound key:', event.which);
-  } else if (event.type == 'keydown') {
-    this.input[key] = true;
-  } else {
-    this.input[key] = false;
-    if (key == 'debug') {
-      this.toggleDebug();
+    if (this.input.dig) {
+      this.player.dig();
     }
   }
-};
 
-Game.prototype.clearInput = function() {
-  console.log('clearing input');
-  for (var key in this.input) {
-    this.input[key] = false;
-  }
-};
-
-Game.prototype.addEntity = function(entity) {
-  entity.id = ++this.lastEntityId;
-  this.entities[entity.id] = entity;
-  this.scene.add(entity.sprite);
-};
-
-Game.prototype.removeEntity = function(entity) {
-  this.scene.remove(entity.sprite);
-  delete this.entities[entity.id];
-};
-
-// Returns local GL coordinates of the center of a block from block coordinates.
-Game.prototype.blockToLocal = function(x, y) {
-  return [
-    (x * BLOCK_SIZE),
-    (y * BLOCK_SIZE)
-  ];
-};
-
-// Returns nearest block coordinates from local GL coordinates. Right and top
-// blocks win on edges.
-Game.prototype.localToBlock = function(x, y) {
-  return [
-    Math.round(x / BLOCK_SIZE),
-    Math.round(y / BLOCK_SIZE)
-  ];
-};
-
-// Returns local GL coordinates on the ground plane from normalized device
-// coordinates.
-Game.prototype.ndcToLocal = function(x, y) {
-  var ndc = new THREE.Vector3(x, y, null);
-  var raycaster = this.projector.pickingRay(ndc, this.camera);
-  return raycaster.ray.intersectPlane(this.groundPlane);
-};
-
-Game.prototype.click = function(event) {
-  if (this.debug) {
-    var lc = this.ndcToLocal(
-      (event.clientX / window.innerWidth) * 2 - 1,
-      -(event.clientY / window.innerHeight) * 2 + 1);
-    var bc = this.localToBlock(lc.x, lc.y);
-    console.log('clicked block', bc);
-    var outline = game.outlineBlock(bc[0], bc[1], 0x00ff00);
-    setTimeout(function() {game.scene.remove(outline)}, 1000);
-  }
-};
-
-Game.prototype.getGroundBeneathEntity = function (entity) {
-  var lc = this.localToBlock(entity.sprite.position.x,
-                             entity.sprite.position.y);
-  var height = lc[1] - 1;
-  while (!this.terrainGrid.has(lc[0], height)) {
-    if (height <= MAX_DEPTH) {
-      return null;
-    }
-    height -= 1;
-  }
-  return this.terrainGrid.get(lc[0], height);
-};
-
-Game.prototype.start = function() {
-  this.player = new Player(this);
-  this.addEntity(this.player);
-
-  var bgController = new BackgroundController(this.scene);
-  bgController.drawBackground();
-
-  window.addEventListener('keydown', this.handleKey.bind(this));
-  window.addEventListener('keyup', this.handleKey.bind(this));
-  window.addEventListener('blur', this.clearInput.bind(this));
-  window.addEventListener('resize', this.resize.bind(this));
-  window.addEventListener('mousedown', this.click.bind(this));
-
-  this.animate();
-}
-
-// Called when when we are allowed to render. In general at 60 fps.
-Game.prototype.animate = function() {
-  this.now = getNow();
-  this.unprocessedFrames += (this.now - this.lastTime) * 60.0 / 1000.0; // 60 fps
-  this.lastTime = this.now;
-  if (this.unprocessedFrames > MAX_CATCHUP) {
-    this.unprocessedFrames = MAX_CATCHUP;
-  }
-  while (this.unprocessedFrames >= 1.0) {
-    this.tick();
-    this.unprocessedFrames -= 1.0;
-  }
-  this.render();
-  requestAnimationFrame(this.animate.bind(this));
-}
-
-// Renders a single frame
-Game.prototype.render = function() {
-  this.renderer.render(this.scene, this.camera);
-};
-
-Game.prototype.generateVisibleWorld = function() {
-  var topLeftLc = this.ndcToLocal(-1, 1);
-  var bottomRightLc = this.ndcToLocal(1, -1);
-  var topLeftBc = this.localToBlock(topLeftLc.x, topLeftLc.y);
-  var bottomRightBc = this.localToBlock(bottomRightLc.x, bottomRightLc.y);
-  this.generateWorld(topLeftBc, bottomRightBc);
-};
-
-Game.prototype.generateWorld = function(topLeft, bottomRight) {
-  this.plants = [];
-  var numNew = 0;
-  for (var x = topLeft[0]; x <= bottomRight[0]; x++) {
-    for (var y = Math.min(topLeft[1], -1); y >= bottomRight[1]; y--) {
-      if (this.terrainGrid.has(x, y)) {
-        continue;
+  handleKey(event:KeyboardEvent) {
+    var key = INPUT_MAP[event.which];
+    if (!key) {
+      console.log('unbound key:', event.which);
+    } else if (event.type == 'keydown') {
+      this.input[key] = true;
+    } else {
+      this.input[key] = false;
+      if (key == 'debug') {
+        this.toggleDebug();
       }
-      numNew++;
-      var ground = new Ground(x, y);
-      this.terrainGrid.set(x, y, ground);
-      this.addEntity(ground);
-      if (y == -1 && Math.random() < 0.5) {
-        var plant = new Plant(x, 0);
-        this.addEntity(plant);
-        this.plants.push(plant);
+    }
+  }
 
-        // Add air around plants
-        this.atmosphereController.addAir(x, 0);
-        var points = Grid.neighbors(x, 0, 2);
-        for (var i = 0; i < points.length; i++) {
-          this.atmosphereController.addAir(points[i][0], points[i][1]);
+  clearInput = function() {
+    console.log('clearing input');
+    for (var key in this.input) {
+      this.input[key] = false;
+    }
+  }
+
+  addEntity = function(entity) {
+    entity.id = ++this.lastEntityId;
+    this.entities[entity.id] = entity;
+    this.scene.add(entity.sprite);
+  }
+
+  removeEntity(entity) {
+    this.scene.remove(entity.sprite);
+    delete this.entities[entity.id];
+  }
+
+  // Returns local GL coordinates of the center of a block from block
+  // coordinates.
+  blockToLocal(x:number, y:number):number[] {
+    return [
+      (x * BLOCK_SIZE),
+      (y * BLOCK_SIZE)
+    ];
+  }
+
+  // Returns nearest block coordinates from local GL coordinates. Right and top
+  // blocks win on edges.
+  localToBlock(x:number, y:number):number[] {
+    return [
+      Math.round(x / BLOCK_SIZE),
+      Math.round(y / BLOCK_SIZE)
+    ];
+  }
+
+  // Returns local GL coordinates on the ground plane from normalized device
+  // coordinates.
+  ndcToLocal(x:number, y:number) {
+    var ndc = new THREE.Vector3(x, y, null);
+    var raycaster = this.projector.pickingRay(ndc, this.camera);
+    return raycaster.ray.intersectPlane(this.groundPlane);
+  }
+
+  click(event:MouseEvent) {
+    if (this.debug) {
+      var lc = this.ndcToLocal(
+        (event.clientX / window.innerWidth) * 2 - 1,
+        -(event.clientY / window.innerHeight) * 2 + 1);
+      var bc = this.localToBlock(lc.x, lc.y);
+      console.log('clicked block', bc);
+      var outline = game.outlineBlock(bc[0], bc[1], 0x00ff00);
+      setTimeout(function() {game.scene.remove(outline)}, 1000);
+    }
+  }
+
+  getGroundBeneathEntity = function (entity) {
+    var lc = this.localToBlock(entity.sprite.position.x,
+                               entity.sprite.position.y);
+    var height = lc[1] - 1;
+    while (!this.terrainGrid.has(lc[0], height)) {
+      if (height <= MAX_DEPTH) {
+        return null;
+      }
+      height -= 1;
+    }
+    return this.terrainGrid.get(lc[0], height);
+  }
+
+  start() {
+    this.player = new Player(this);
+    this.addEntity(this.player);
+
+    var bgController = new BackgroundController(this.scene);
+    bgController.drawBackground();
+
+    window.addEventListener('keydown', this.handleKey.bind(this));
+    window.addEventListener('keyup', this.handleKey.bind(this));
+    window.addEventListener('blur', this.clearInput.bind(this));
+    window.addEventListener('resize', this.resize.bind(this));
+    window.addEventListener('mousedown', this.click.bind(this));
+
+    this.animate();
+  }
+
+  // Called when when we are allowed to render. In general at 60 fps.
+  animate() {
+    this.now = getNow();
+    this.unprocessedFrames += (this.now - this.lastTime) * 60.0 / 1000.0; // 60 fps
+    this.lastTime = this.now;
+    if (this.unprocessedFrames > MAX_CATCHUP) {
+      this.unprocessedFrames = MAX_CATCHUP;
+    }
+    while (this.unprocessedFrames >= 1.0) {
+      this.tick();
+      this.unprocessedFrames -= 1.0;
+    }
+    this.render();
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  // Renders a single frame
+  render() {
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  generateVisibleWorld() {
+    var topLeftLc = this.ndcToLocal(-1, 1);
+    var bottomRightLc = this.ndcToLocal(1, -1);
+    var topLeftBc = this.localToBlock(topLeftLc.x, topLeftLc.y);
+    var bottomRightBc = this.localToBlock(bottomRightLc.x, bottomRightLc.y);
+    this.generateWorld(topLeftBc, bottomRightBc);
+  }
+
+  generateWorld(topLeft, bottomRight) {
+    this.plants = [];
+    var numNew = 0;
+    for (var x = topLeft[0]; x <= bottomRight[0]; x++) {
+      for (var y = Math.min(topLeft[1], -1); y >= bottomRight[1]; y--) {
+        if (this.terrainGrid.has(x, y)) {
+          continue;
+        }
+        numNew++;
+        var ground = new Ground(x, y);
+        this.terrainGrid.set(x, y, ground);
+        this.addEntity(ground);
+        if (y == -1 && Math.random() < 0.5) {
+          var plant = new Plant(x, 0);
+          this.addEntity(plant);
+          this.plants.push(plant);
+
+          // Add air around plants
+          this.atmosphereController.addAir(x, 0);
+          var points = Grid.neighbors(x, 0, 2);
+          for (var i = 0; i < points.length; i++) {
+            this.atmosphereController.addAir(points[i][0], points[i][1]);
+          }
         }
       }
     }
+    if (this.debug && numNew > 0) {
+      console.log('generated', numNew, 'blocks',
+                  'from', topLeft, 'to', bottomRight);
+    }
   }
-  if (this.debug && numNew > 0) {
-    console.log('generated', numNew, 'blocks',
-                'from', topLeft, 'to', bottomRight);
-  }
-};
 
-Game.prototype.onGround = function(entity) {
-  var ground = this.getGroundBeneathEntity(entity);
-  if (!ground) {
-    return false;
+  onGround(entity) {
+    var ground = this.getGroundBeneathEntity(entity);
+    if (!ground) {
+      return false;
+    }
+    return entity.sprite.position.y - (ground.sprite.position.y + 74) < 1;
   }
-  return entity.sprite.position.y - (ground.sprite.position.y + 74) < 1;
-};
 
-var tickCount = 0;
-// Single tick of game time (1 frame)
-Game.prototype.tick = function() {
-  this.handleInput();
-  if (tickCount == 0) {
+  // Single tick of game time (1 frame)
+  tick() {
+    this.handleInput();
+    if (tickCount == 0) {
+      this.generateVisibleWorld();
+    }
+    for (var id in this.entities) {
+      this.entities[id].tick();
+    }
+    for (var bc in this.terrainGrid._grid) {
+      this.terrainGrid._grid[bc].tick();
+    }
+    tickCount++;
+  }
+
+  panCamera(x?:number, y?:number) {
+    if (x != null) {
+      this.camera.position.x += x;
+    }
+    if (y != null) {
+      this.camera.position.y += y;
+    }
     this.generateVisibleWorld();
   }
-  for (var id in this.entities) {
-    this.entities[id].tick();
-  }
-  for (var bc in this.terrainGrid._grid) {
-    this.terrainGrid._grid[bc].tick();
-  }
-  tickCount++;
-};
 
-Game.prototype.panCamera = function(x, y) {
-  if (x != null) {
-    this.camera.position.x += x;
+  toggleDebug() {
+    this.debug = !this.debug;
+
+    if (this.debug) {
+      this.debugSprites = [];
+
+      // Origin block.
+      this.debugSprites.push(
+        this.outlineBlock(0, 0, 0x0000ff));
+
+      // Origin lines.
+      this.debugSprites.push(
+        this.drawLine([0, 300], [0, -300], 0xff0000));
+      this.debugSprites.push(
+        this.drawLine([300, 0], [-300, 0], 0xff0000));
+
+    } else {
+      var self = this;
+      this.debugSprites.forEach(function(sprite) {
+        self.scene.remove(sprite);
+      });
+    }
   }
-  if (y != null) {
-    this.camera.position.y += y;
+
+  drawLine(from, to, color) {
+    var material = new THREE.LineBasicMaterial({color: color || null});
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(from[0], from[1], 1));
+    geometry.vertices.push(new THREE.Vector3(to[0], to[1], 1));
+    var line = new THREE.Line(geometry, material);
+    this.scene.add(line);
+    return line;
   }
-  this.generateVisibleWorld();
+
+  drawRect(cornerA, cornerB, color) {
+    var material = new THREE.LineBasicMaterial({color: color || null});
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(cornerA[0], cornerA[1], 1));
+    geometry.vertices.push(new THREE.Vector3(cornerB[0], cornerA[1], 1));
+    geometry.vertices.push(new THREE.Vector3(cornerB[0], cornerB[1], 1));
+    geometry.vertices.push(new THREE.Vector3(cornerA[0], cornerB[1], 1));
+    geometry.vertices.push(new THREE.Vector3(cornerA[0], cornerA[1], 1));
+    var box = new THREE.Line(geometry, material);
+    this.scene.add(box);
+    return box;
+  }
+
+  outlineBlock(x, y, color) {
+    var lc = this.blockToLocal(x, y);
+    return this.drawRect(
+      [lc[0] - BLOCK_SIZE / 2, lc[1] - BLOCK_SIZE / 2],
+      [lc[0] + BLOCK_SIZE / 2, lc[1] + BLOCK_SIZE / 2],
+      color);
+  }
 }
-
-Game.prototype.toggleDebug = function() {
-  this.debug = !this.debug;
-
-  if (this.debug) {
-    this.debugSprites = [];
-
-    // Origin block.
-    this.debugSprites.push(
-      this.outlineBlock(0, 0, 0x0000ff));
-
-    // Origin lines.
-    this.debugSprites.push(
-      this.drawLine([0, 300], [0, -300], 0xff0000));
-    this.debugSprites.push(
-      this.drawLine([300, 0], [-300, 0], 0xff0000));
-
-  } else {
-    var self = this;
-    this.debugSprites.forEach(function(sprite) {
-      self.scene.remove(sprite);
-    });
-  }
-};
-
-Game.prototype.drawLine = function(from, to, color) {
-  var material = new THREE.LineBasicMaterial({color: color || null});
-  var geometry = new THREE.Geometry();
-  geometry.vertices.push(new THREE.Vector3(from[0], from[1], 1));
-  geometry.vertices.push(new THREE.Vector3(to[0], to[1], 1));
-  var line = new THREE.Line(geometry, material);
-  this.scene.add(line);
-  return line;
-};
-
-Game.prototype.drawRect = function(cornerA, cornerB, color) {
-  var material = new THREE.LineBasicMaterial({color: color || null});
-  var geometry = new THREE.Geometry();
-  geometry.vertices.push(new THREE.Vector3(cornerA[0], cornerA[1], 1));
-  geometry.vertices.push(new THREE.Vector3(cornerB[0], cornerA[1], 1));
-  geometry.vertices.push(new THREE.Vector3(cornerB[0], cornerB[1], 1));
-  geometry.vertices.push(new THREE.Vector3(cornerA[0], cornerB[1], 1));
-  geometry.vertices.push(new THREE.Vector3(cornerA[0], cornerA[1], 1));
-  var box = new THREE.Line(geometry, material);
-  this.scene.add(box);
-  return box;
-};
-
-Game.prototype.outlineBlock = function(x, y, color) {
-  var lc = this.blockToLocal(x, y);
-  return this.drawRect(
-    [lc[0] - BLOCK_SIZE / 2, lc[1] - BLOCK_SIZE / 2],
-    [lc[0] + BLOCK_SIZE / 2, lc[1] + BLOCK_SIZE / 2],
-    color);
-};
-
 
 var game;
 window.addEventListener('load', function() {
--- player.ts
+++ player.ts
@@ -1,101 +1,103 @@
 var DUDE_MATERIAL = LoadJaggyMaterial('images/dude.png');
 var FLASH_MATERIAL = LoadJaggyMaterial('images/flash.png')
 
-function Player (game) {
-  this.speedX = 0;
-  this.speedY = 0;
-  this.sprite = new THREE.Sprite(DUDE_MATERIAL);
-  this.teleport(0, 20);
-  this.sprite.scale.set(4*13, 4*21, 1.0); // imageWidth, imageHeight
-  this.direction = -1;
-  this.game = game;
-  this.lastDig = -10000;
+class Player {
+  speedX = 0;
+  speedY = 0;
+  sprite = new THREE.Sprite(DUDE_MATERIAL);
+  direction = -1;
+  lastDig = -10000;
+  flashSprite = new THREE.Sprite(FLASH_MATERIAL);
 
-  this.flashSprite = new THREE.Sprite(FLASH_MATERIAL);
-  this.flashSprite.scale.set(4*13, 4*21, 1.0); // imageWidth, imageHeight
-};
-
-Player.prototype.tick = function() {
-  // Move camera with player
-  if (this.sprite.position.x - this.game.camera.position.x > 300) {
-    // Pan right with player
-    this.game.panCamera(Math.abs(this.speedX));
-  } else if (this.sprite.position.x - this.game.camera.position.x < -300) {
-    // Pan left with player
-    this.game.panCamera(-Math.abs(this.speedX));
+  constructor(public game:Game) {
+    this.teleport(0, 20);
+    this.sprite.scale.set(4*13, 4*21, 1.0); // imageWidth, imageHeight
+    this.flashSprite.scale.set(4*13, 4*21, 1.0); // imageWidth, imageHeight
   }
 
-  // Gravity on player
-  if (!this.game.onGround(this)) {
-    this.speedY -= 0.7;
-  }
+  tick() {
+    // Move camera with player
+    if (this.sprite.position.x - this.game.camera.position.x > 300) {
+      // Pan right with player
+      this.game.panCamera(Math.abs(this.speedX));
+    } else if (this.sprite.position.x - this.game.camera.position.x < -300) {
+      // Pan left with player
+      this.game.panCamera(-Math.abs(this.speedX));
+    }
 
-  // apply speed to position
-  this.sprite.position.x += this.speedX;
+    // Gravity on player
+    if (!this.game.onGround(this)) {
+      this.speedY -= 0.7;
+    }
 
-  var groundBeneath = this.game.getGroundBeneathEntity(this);
-  this.sprite.position.y += this.speedY;
-  var newGroundBeneath = this.game.getGroundBeneathEntity(this);
-  if (groundBeneath != newGroundBeneath) {
-    // collide with old ground beneath
-    // we went through groundBeneith, so reset our height to be its.
-    this.sprite.position.y = Math.max(groundBeneath.sprite.position.y, newGroundBeneath.sprite.position.y) + 74;
-    this.speedY = 0;
-  }
+    // apply speed to position
+    this.sprite.position.x += this.speedX;
 
-  // facing
-  if ((this.direction == -1 && this.speedX > 0) ||
-      (this.direction == 1 && this.speedX < 0)) {
-    this.sprite.scale.x *= -1;
-    this.direction *= -1
-  }
+    var groundBeneath = this.game.getGroundBeneathEntity(this);
+    this.sprite.position.y += this.speedY;
+    var newGroundBeneath = this.game.getGroundBeneathEntity(this);
+    if (groundBeneath != newGroundBeneath) {
+      // collide with old ground beneath
+      // we went through groundBeneith, so reset our height to be its.
+      this.sprite.position.y = Math.max(groundBeneath.sprite.position.y, newGroundBeneath.sprite.position.y) + 74;
+      this.speedY = 0;
+    }
 
-  // friction
-  if (this.speedX) {
-    this.speedX *= 0.92;
-    if (Math.abs(this.speedX) < 0.50) {
-      this.speedX = 0;
+    // facing
+    if ((this.direction == -1 && this.speedX > 0) ||
+        (this.direction == 1 && this.speedX < 0)) {
+      this.sprite.scale.x *= -1;
+      this.direction *= -1
+    }
+
+    // friction
+    if (this.speedX) {
+      this.speedX *= 0.92;
+      if (Math.abs(this.speedX) < 0.50) {
+        this.speedX = 0;
+      }
+    }
+
+    // Highlight the trail of blocks we enter.
+    if (game.debug) {
+      var bc = game.localToBlock(this.sprite.position.x,
+                                    this.sprite.position.y);
+      var outline = game.outlineBlock(bc[0], bc[1]);
+      setTimeout(function() {game.scene.remove(outline)}, 500);
     }
   }
 
-  // Highlight the trail of blocks we enter.
-  if (game.debug) {
-    var bc = game.localToBlock(this.sprite.position.x,
-                                  this.sprite.position.y);
-    var outline = game.outlineBlock(bc[0], bc[1]);
-    setTimeout(function() {game.scene.remove(outline)}, 500);
+  teleport(x:number, y:number) {
+    var lc = game.blockToLocal(x, y);
+    this.sprite.position.set(lc[0], lc[1], 0);
   }
-};
 
-Player.prototype.teleport = function(x, y) {
-  var lc = game.blockToLocal(x, y);
-  this.sprite.position.set(lc[0], lc[1], 0);
-};
-
-Player.prototype.jump = function() {
-  if (this.game.onGround(this)) {
-    this.speedY = 13;
+  jump() {
+    if (this.game.onGround(this)) {
+      this.speedY = 13;
+    }
   }
-};
 
-Player.prototype.dig = function() {
-  var now = getNow();
-  if (now - 650 < this.lastDig) {
-    return;
+  dig() {
+    var now = getNow();
+    if (now - 650 < this.lastDig) {
+      return;
+    }
+    this.lastDig = now;
+
+    var flashSprite = this.flashSprite;
+    flashSprite.position.set(this.sprite.position.x,
+                             this.sprite.position.y - 30,
+                             1);
+    game.scene.add(flashSprite);
+    setTimeout(function() {
+      game.scene.remove(flashSprite);
+    }, 100);
+
+    var ground = this.game.getGroundBeneathEntity(this);
+    if (ground) {
+      ground.hit();
+    }
   }
-  this.lastDig = now;
+}
 
-  var flashSprite = this.flashSprite;
-  flashSprite.position.set(this.sprite.position.x,
-                           this.sprite.position.y - 30,
-                           1);
-  game.scene.add(flashSprite);
-  setTimeout(function() {
-    game.scene.remove(flashSprite);
-  }, 100);
-
-  var ground = this.game.getGroundBeneathEntity(this);
-  if (ground) {
-    ground.hit();
-  }
-};
