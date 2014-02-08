37fc4d16ee5a184148af9d9e37c162e434212909
Peter Burns <rictic@google.com>
2014-02-08 11:41:12 -0800

    add a forEach function to Grid

--- grid.ts
+++ grid.ts
@@ -12,6 +12,12 @@ class Grid<T> {
   clear(x:number, y:number) {
     delete this._grid['' + [x, y]];
   }
+  forEach(f:(x:number, y:number, value:T)=>void) {
+    for (var key in this._grid) {
+      var s = key.split(',');
+      f(parseInt(s[0], 10), parseInt(s[1], 10), this._grid[key]);
+    }
+  }
 
   // Returns list of neighboring grid coordinates. If range is passed then
   // returns list of neighbors withing Manhattan distance range.
