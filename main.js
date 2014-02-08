39bad3de12c7a7a94cd19a99c07a722d2b0651a5
Alexander Marks <aomarks@gmail.com>
2014-02-08 14:12:49 -0800

    automatic commit of compiled js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var PLAYER_MAX_SPEED = 8;
+var PLAYER_ACCELERATION = 0.001;
+var JUMP_HEIGHT = 10;
+var MAX_DEPTH = -6;
+var MAX_CATCHUP = 10;
+var BLOCK_SIZE = 32;
+var MAGIC_NUMBER = 56;
 var Grid = (function () {
     function Grid() {
         this._grid = {};
@@ -366,20 +373,13 @@ var BackgroundController = (function () {
     return BackgroundController;
 })();
 /// <reference path='lib/three.d.ts'/>
+/// <reference path='consts.ts'/>
 /// <reference path='grid.ts'/>
 /// <reference path='ground.ts'/>
 /// <reference path='atmosphere.ts'/>
 /// <reference path='plant.ts'/>
 /// <reference path='player.ts'/>
 /// <reference path='background.ts'/>
-var PLAYER_MAX_SPEED = 8;
-var PLAYER_ACCELERATION = 0.001;
-var JUMP_HEIGHT = 10;
-var MAX_DEPTH = -6;
-var MAX_CATCHUP = 10;
-var BLOCK_SIZE = 32;
-var MAGIC_NUMBER = 56;
-
 var INPUT_MAP = {
     87: 'jump',
     83: 'down',
